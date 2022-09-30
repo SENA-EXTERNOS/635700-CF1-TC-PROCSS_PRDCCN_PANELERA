2d440077d08adc2c2a60bd385e3f21c0cdcc25ab
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-30 17:26:10 -0500

    Enlaces tema 1.2
    Se agregan enlaces del tema 1.2 que se encuentran en la línea de tiempo

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -15,7 +15,7 @@
         p.mb-0 En Colombia la cultura gastronómica emplea la panela para la producción de alimentos, se encuentra en: bebidas, postres, platos fuertes, en otros; por ello la importancia no sólo de conocer la variedad de usos como producto natural orgánico de alta fuente nutritiva, si no también entender la producción, abarcando los requerimientos para el procesamiento, embalaje y almacenamiento.
         
     Separador
-    
+
     #t_1_1.titulo-segundo.color-acento-contenido.mb-5
       h2 1.1 Interpretación de documentos
 
@@ -45,7 +45,7 @@
           p Por la cual se establece el reglamento técnico de emergencia, se señalan los requisitos sanitarios que se deben cumplir en la producción y comercialización de la panela para el consumo humano y se dictan otras disposiciones, en la cual se determina que la panela es un producto con un importante valor nutricional, de alto uso especialmente en la dieta de la población infantil y la de bajos recursos.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.minsalud.gov.co/Normatividad_Nuevo/RESOLUCI%C3%93N%202546%20DE%202004.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -64,7 +64,7 @@
           p Hace referencia a la reglamentación en los máximos de producción y las características del negocio para los productos elaborados a base de panela y mieles vírgenes y sus derivados por campesinos y/o artesanos dentro de la categoría (A) – artesanal y se dictan otras disposiciones.
           .row
             .col-lg-6.text-start
-              a.anexo.mb-3.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-3.mb-lg-2.mr-3-custom(href="https://www.minagricultura.gov.co/Normatividad/Resoluciones/RESOLUCI%C3%93N%20NO.%20000247%20DE%202021.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -77,7 +77,7 @@
           p El contenido reglamenta parcialmente l Título I de la Ley 09 de 1979, así como el Capítulo II del Título VI - Parte III - Libro II y el Título III de la Parte III Libro I del Decreto 2811 de 1974 en cuanto a usos del agua y residuos líquidos.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href=" https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=18617" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -96,7 +96,7 @@
           p La normativa en mención, fija un precio de referencia para la liquidación de la cuota de fomento panelero para el segundo semestre de 2020.
           .row
             .col-lg-6.text-start
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.minagricultura.gov.co/Normatividad/Resoluciones/RESOLUCI%C3%93N%20154-2020%20PRECIO%20DE%20REFERENCIA%20CUOTA%20DE%20FOMENTO%20PANELERO.pdf " target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -110,7 +110,7 @@
           p El texto dictamina el apoyo que se otorga al transporte de mieles paneleras producidas en algunas zonas del país.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://xperta.legis.co/visor/legcol/legcol_8bc1596095ad20c6e0430a01015120c6/coleccion-de-legislacion-colombiana/resolucion-258-de-2010" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -151,7 +151,7 @@
           p El presente proyecto de ley busca generar incentivos tendientes a ampliar la demanda de panela y mieles vírgenes, así como diversificar la producción y comercialización de sus derivados. De igual forma, se dictan disposiciones adicionales con el fin de proteger y fortalecer, de manera especial, la producción y el bienestar de pequeños y medianos productores.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href=" https://www.studocu.com/co/document/instituto-universitario-de-la-paz/metodologia-de-investigacion-social-cualitativa-1/ley-2005-del-02-de-diciembre-de-2019-panela/22665699" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -170,7 +170,7 @@
           p Se centra en la reglamentación parcial, la Ley 23 de 1973, los artículos 33, 73, 74, 75 y 76 del Decreto - Ley 2811 de 1974; los artículos 41, 42, 43, 44, 45, 48 y 49 de la Ley 9 de 1979; y la Ley 99 de 1993, en relación con la prevención y control de la contaminación atmosférica y la protección de la calidad del aire.
           .row
             .col-lg-6.text-start
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.icbf.gov.co/cargues/avance/docs/decreto_0948_1995.htm" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -183,7 +183,7 @@
           p Por la cual se establece el reglamento técnico sobre los requisitos sanitarios que se deben cumplir en la producción y comercialización de la panela para consumo humano y se dictan otras disposiciones. Dicha disposición fue modificada por el Gobierno Nacional mediante la Resolución 4121 de 2011, pero en ambos documentos se resalta la importancia de los espacios debidamente aseados y los insumos que garanticen la calidad del producto. A continuación, exponemos algunas de las normas que rigen a los trapiches colombianos.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href=" https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/DE/DIJ/Resolucion-779-de-2006.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -202,7 +202,7 @@
           p Por la cual se modifica parcialmente la Resolución 779 de 2006, modificadas por las Resoluciones 3462 de 2008 y 3544 de 2009.
           .row
             .col-lg-6.text-start
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.minsalud.gov.co/Normatividad_Nuevo/RESOLUCI%C3%93N%204121%20DE%202011.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -216,7 +216,7 @@
           p Por la cual se modifican los artículos 11 y 13 de la resolución 779 de 2006.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href=" https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/DE/DIJ/Resolucion-3544-de-2009.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -235,7 +235,7 @@
           p Por la cual se modifica el parágrafo del artículo 9° y el artículo 15 de la Resolución 779 de 2006 y se dictan otras disposiciones.
           .row
             .col-lg-6.text-start
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.mincit.gov.co/getattachment/7385e884-376c-4a42-87fd-65e47215c1f5/Resolucion-16379-de-2003-Por-la-cual-se-reglamenta.aspx" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
@@ -248,7 +248,7 @@
           p Por la cual se establece el reglamento técnico sobre los requisitos de rotulado o etiquetado que deben cumplir los alimentos envasados y materias primas de alimentos para consumo humano.
           .row
             .col-lg-6
-              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.google.com/?hl=es" target="_blank")
+              a.anexo.mb-4.mb-lg-2.mr-3-custom(href="https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/DE/DIJ/Resoluci%C3%B3n_0485_de_2005.pdf" target="_blank")
                 .anexo__icono
                   img(src="@/assets/template/icono-link.svg")
                 .anexo__texto
